6b4e46e5db982d392b636dcb311f06df645bec76
arnavarora0003 <arnavarora0003@gmail.com>
2024-10-05 16:16:39 +0000

    about and contact pages

--- client/src/app/api/auth/signout/route.ts
+++ client/src/app/api/auth/signout/route.ts
@@ -1,14 +1,15 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { createSessionClient } from '@/lib/appwrite';
 
-export async function POST() {  
+export async function POST(req: NextRequest) {
   try {
+    console.log(`Request method: ${req.method}, Request URL: ${req.url}`);
     const { account } = await createSessionClient();
     await account.deleteSession('current');
     return NextResponse.json({ message: 'Signed out successfully' }, { status: 200 });
-  } catch {
-    return NextResponse.json({ message: 'Error signing out' }, { status: 500 }); 
+  } catch  {
+    return NextResponse.json({ message: 'Error signing out' }, { status: 500 });
   }
 }
 
-export const runtime = "edge";
+export const runtime = "edge"
